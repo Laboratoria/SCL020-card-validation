f5fb22e919544e431806143525f05621106d4def
ConiVC96 <constanzavc.96@outlook.cl>
2022-03-28 10:43:04 -0300

    Cree y corregí la segunda página

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import validator from './validator.js';
+$('form').card({
+    container: '.card-wrapper',
+    width: 280,
 
-console.log(validator);
+    formSelectors: {
+        nameInput: 'input[name="first-name"], input[name="last-name"]'
+    }
+});
