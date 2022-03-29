03725e1ea63cd9236e28fa184cd4749a2aa6ec01
Camila Arredondo Cartagena <camiarredondo@MacBook-Air-de-Camila.local>
2022-03-29 13:37:26 -0300

    maskify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
+
 import validator from './validator.js';
 
 document.getElementById("validar").addEventListener('click', () => {
+
+
+  let digitos = document.getElementById('ntarjeta').value;
+  if (digitos=="") {
+    alert ('Debe ingresar numero de tarjeta');
+    return;
+  }
+
+  let nombreyapellido = document.getElementById('nombre').value;
+  if (nombreyapellido=="") {
+  alert ('Debe ingresar Nombre y Apellido');
+  return;
+
+  }
+
+  let fven = document.getElementById('fechavencimiento').value;
+  if (fven=="") {
+  alert ('Debe ingresar Fecha de Vencimiento');
+  return;
+
+  }
+
+  let codigo = document.getElementById('digito').value;
+  if (codigo=="") {
+  alert ('Debe ingresar CVV');
+  return;
+
+  }
+
 
   let nusuario = document.getElementById('nombre').value;
 
@@ -11,10 +41,8 @@ document.getElementById("validar").addEventListener('click', () => {
   let esvalida = validator.isValid(valortarjeta);
 
   if (esvalida) {
-    alert('Tarjeta de Credito Valida, ' + nusuario + ' su reserva se ha realizado exitosamente.');
+    alert( validator.maskify(valortarjeta) + ' Tarjeta de Credito Valida, ' + nusuario + ' su reserva se ha realizado exitosamente.');
   } else {
-    alert('Tarjeta de Credito Invalida, ' + nusuario + ' intente nuevamente.')
+    alert( validator.maskify(valortarjeta) + ' Tarjeta de Credito Invalida, ' + nusuario + ' intente nuevamente.')
   }
-
-
 });
